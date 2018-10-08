d82c3874de3316d9d1ed0142ddf772f8fdf94497
sunnysonu <sunnysonu582@gmail.com>
2018-10-08 14:36:45 +0530

    16

--- app/serializers/ChefProfile.py
+++ app/serializers/ChefProfile.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.settings import api_settings
+from rest_framework_jwt.settings import api_settings
 from app.serializers.User import *
 from app.models import *
 
