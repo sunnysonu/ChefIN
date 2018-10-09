bc260c1cc2c58cb329784c2689f1d8b2657e9722
sunnysonu <sunnysonu582@gmail.com>
2018-10-09 20:23:20 +0530

    added slash to urls

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,8 @@ app_name = "chefin"
 urlpatterns = [
     path("user/", UserProfileSignup.as_view(), name="user_signup"),
     path("chef/", ChefProfileSignup.as_view(), name = "chef_signup"),
-    path("user/<int:pk>", UserDetailProfile.as_view(), name = "user"),
-    path("chef/<int:pk>", ChefDetailProfile.as_view(), name = "chef"),
+    path("user/<int:pk>/", UserDetailProfile.as_view(), name = "user"),
+    path("chef/<int:pk>/", ChefDetailProfile.as_view(), name = "chef"),
     path("", EventRequestListAPIView.as_view(), name = "EventList"),
     path("<int:pk>/", EventDetailAPIView.as_view(), name = "EventDetail"),
 ]
