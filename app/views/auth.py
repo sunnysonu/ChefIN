a450392ea833e36ace53a1c85fdcf06133d4424b
sunnysonu <sunnysonu582@gmail.com>
2018-11-18 16:12:59 +0530

    Changed token return value

--- app/views/auth.py
+++ app/views/auth.py
@@ -6,5 +6,6 @@ def jwt_response_payload_handler(token, user=None, request=None):
     """
     return {
         'token': token,
-        'username' : user.username
+        'username' : user.username,
+        "id" : user.username,
     }
