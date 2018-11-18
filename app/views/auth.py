ade6ab5cb507b7179f6ee3b0f833a34d070bdd29
sunnysonu <sunnysonu582@gmail.com>
2018-11-18 16:15:25 +0530

    Same as previous commit

--- app/views/auth.py
+++ app/views/auth.py
@@ -7,5 +7,5 @@ def jwt_response_payload_handler(token, user=None, request=None):
     return {
         'token': token,
         'username' : user.username,
-        "id" : user.username,
+        "id" : user.id,
     }
